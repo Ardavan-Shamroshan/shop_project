c83d9b555ccbe54eaa1d211a19b34454275e681c
ardavanshamroshan <ardavan2000.ashr@gmail.com>
2024-02-01 13:20:04 +0330

    scheduling auto send email using custom command and Console/Kernel has been done and activating crone job in server is in progress

---
+++ app/Console/Commands/AutoEmail.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\Email\SendEmailToUsersJob;
+use App\Models\Notify\Email;
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class AutoEmail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'auto:send-email';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send scheduling mail automatically';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $emailsToSend = Email::query()->where('published_at', '=', now())->get();
+        $emailsToSend->map(function ($email) {
+            SendEmailToUsersJob::dispatch(
+                User::whereNotNull('email')->get(),
+                $email
+            );
+        });
+    }
+}
